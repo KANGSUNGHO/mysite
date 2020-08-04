6f493d374fc804017fbf9c0be35acb2b39dc2094
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-08-04 18:01:02 +0900

    mysite03 spring 예외처리

---
+++ mysite03/src/main/java/com/bit2020/mysite/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.bit2020.mysite.exception;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice //advice가 나오면 aop라고 생각하면됨. 바깥으로 뺀다.
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public void handlerException(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Exception e) throws Exception {
+		
+		// 1. 로깅 작업 (logging)
+		System.out.println("error:" + e);
+		
+		// 2. 안내 페이지 - 사과 페이지 
+		request.setAttribute("exception", e.toString());
+		request.getRequestDispatcher("/WEB-INF/views/error/exception.jsp").forward(request, response);
+	}
+}
