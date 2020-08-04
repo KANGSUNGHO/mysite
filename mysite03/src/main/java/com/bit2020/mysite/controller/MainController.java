9cc0e9b0a3f4c907358e57b92b51e6e6b2a20397
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-08-04 11:52:34 +0900

    viewResolvor 설정으로 인한 MainController return문 변경

--- mysite03/src/main/java/com/bit2020/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/bit2020/mysite/controller/MainController.java
@@ -8,7 +8,7 @@ public class MainController {
 	
 	@RequestMapping("")
 	public String index() {
-		return "/WEB-INF/views/main/index.jsp";
+		return "main/index"; // 원래는 "/WEB-INF/views/main/index.jsp" 이걸로 return해야 하는데 view resolve에서 prefix, suffix로 앞뒤를 설정해놨기 때문. 
 	}
 
 }
