95257e7b65558e7af6db2b659f98b175dfd54a8c
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-08-05 14:59:10 +0900

    logback logging 처리

--- mysite03/src/main/java/com/bit2020/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/bit2020/mysite/exception/GlobalExceptionHandler.java
@@ -3,12 +3,16 @@ package com.bit2020.mysite.exception;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice //advice가 나오면 aop라고 생각하면됨. 바깥으로 뺀다.
 public class GlobalExceptionHandler {
 
+	private static final Log LOG = LogFactory.getLog(GlobalExceptionHandler.class);
+	
 	@ExceptionHandler(Exception.class)
 	public void handlerException(
 			HttpServletRequest request,
@@ -16,7 +20,7 @@ public class GlobalExceptionHandler {
 			Exception e) throws Exception {
 		
 		// 1. 로깅 작업 (logging)
-		System.out.println("error:" + e);
+		LOG.error("error:" + e);
 		
 		// 2. 안내 페이지 - 사과 페이지 
 		request.setAttribute("exception", e.toString());
