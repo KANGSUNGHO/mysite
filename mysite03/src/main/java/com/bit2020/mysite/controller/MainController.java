f50a8dda2679bd52c2e364d27f8102332b9ed9cc
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-08-15 02:51:08 +0900

    @ResponseBody 주석문 추가

--- mysite03/src/main/java/com/bit2020/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/bit2020/mysite/controller/MainController.java
@@ -14,7 +14,7 @@ public class MainController {
 		return "main/index"; // 원래는 "/WEB-INF/views/main/index.jsp" 이걸로 return해야 하는데 view resolve에서 prefix, suffix로 앞뒤를 설정해놨기 때문. 
 	}
 
-	@ResponseBody
+	@ResponseBody //@ResponseBody를 붙이지 않으면 이것을 ViewResolver가 받는데 @ResponsesBody를 붙이면 메시지 컨버터가 받아서 HTML로 응답. 
 	@RequestMapping("/hello")
 	public String hello() {
 		return "안녕하세요~";
