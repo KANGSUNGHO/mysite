c92d3b0deb37f7d4e1c19f7f621f328b7cf8f161
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-08-14 16:17:29 +0900

    AOP 적용 부분 주석 추가

--- mysite03/src/main/java/com/bit2020/mysite/aspect/MeasureExecutionTimeAspect.java
+++ mysite03/src/main/java/com/bit2020/mysite/aspect/MeasureExecutionTimeAspect.java
@@ -10,11 +10,11 @@ import org.springframework.util.StopWatch;
 @Aspect
 @Component
 public class MeasureExecutionTimeAspect {
-	
+	 // 리턴타입은 모든 리턴타입, com.bit2020.mysite.repository, controller, service에 들어있는 모든 클래스와 그 안에 들어있는 모든 메소드.
 	@Around("execution(* *..*.repository.*.*(..)) || execution(* *..*.controller.*.*(..)) || execution(* *..*.service.*.*(..))")
 	public Object aroundAspect(ProceedingJoinPoint pjp) throws Throwable {
-		// before
 		
+		// before
 		StopWatch sw = new StopWatch();
 		sw.start();
 		
